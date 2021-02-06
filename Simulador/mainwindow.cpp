b8832c50f4df131a33121a3eae15df5f22bae1f4
Oscar Gutiérrez <oscar.gutierrezr@udea.edu.co>
2021-02-05 20:47:55 -0500

    Desarrollo y pruebas en el punto 4, usando código del punto 3

--- Simulador/mainwindow.cpp
+++ Simulador/mainwindow.cpp
@@ -207,7 +207,73 @@ void MainWindow::Simulacion_3(float Ang, float VO0)
     }
 }
 
+void MainWindow::Simulacion_4(float Ang, float VO0)
+{
+    int flag = 0;
+    bool flag2;
+    float xf,yf, x2,y2;
+    float aux,auy;
+    float Vxo,Vy0, Vxoo,Vyoo;
+    int V0o = 0;
+    int Time = 2;
+    float angulo = 0;
+    Vxoo = VO0*cos((Ang)*pi/180);
+    Vyoo = VO0*sin((Ang)*pi/180);
+    for(V0o = 0;V0o<500; V0o +=5){
+        for(angulo = 0; angulo < 90; angulo++){
+            Vxo = V0o*cos((angulo+90)*pi/180);
+            Vy0 = V0o*sin((angulo+90)*pi/180);
+            xf = 0.0;
+            yf = 0.0;
+            x2 = 0.0;
+            y2 = 0.0;
+            for(int t=0; ; t++){
+                xf = Canones.at(1)->getPosx()+Vxo*t;
+                yf = (v_limit-Canones.at(1)->getPosy()) + Vy0*t -(0.5*G*t*t);
+                x2 = Canones.at(0)->getPosx()+Vxoo*(t+Time);
+                y2 = (v_limit-Canones.at(0)->getPosy()) + Vyoo*(t+Time) -(0.5*G*(t+Time)*(t+Time));
+                for(int t2 = t; ;t2++){
+                    aux = Canones.at(1)->getPosx()+Vxo*t2;
+                    auy = Canones.at(1)->getPosy() + Vy0*t2 -(0.5*G*t2*t2);
+                    if(Impacto(x2,y2,aux,auy,Canones.at(2)->getR())){
+                        flag2 = 1;
+                        break;
+                    }
+                    if(auy < 0){
+                        break;
+                    }
+                }
+                if(flag2){
+                    flag2 = 0;
+                    break;
+                }
+                if(Impacto(xf,yf,Canones.at(0)->getPosx(),Canones.at(0)->getPosy(),Canones.at(2)->getR())){
+                    break;
+                }
 
+                if(Impacto(xf,yf,x2,y2,Canones.at(2)->getR())){
+                    Disparos.push_back(new Proyectil_Graph(Canones.at(1)->getPosx(),v_limit-Canones.at(1)->getPosy(),angulo+90,V0o,Canones.at(1)->getPosx(),2));
+                    scene->addItem(Disparos.back());
+                    Disparos.push_back(new Proyectil_Graph(Canones.at(1)->getPosx(),v_limit-Canones.at(1)->getPosy(),angulo+90,V0o,Canones.at(1)->getPosx(),4));
+                    scene->addItem(Disparos.back());
+                    flag += 1;
+                    V0o += 50;
+                    break;
+                }
+                if(yf < 0){
+                    break;
+                }
+            }
+            if(flag == 3) break;
+
+        }
+        if(flag == 3) break;
+    }
+    if(flag < 3){
+        msgBox.setText("No se encontraron al menos 3 disparos efectivos");
+        msgBox.exec();
+    }
+}
 
 void MainWindow::on_pushButton_clicked()
 {
@@ -279,6 +345,14 @@ void MainWindow::on_pushButton_6_clicked()
     }
     else if(Seed==3){
         Simulacion_3(ui->doubleSpinBox_2->value(),ui->doubleSpinBox->value());
+        Disparos.push_back(new Proyectil_Graph(Canones.at(0)->getPosx(),v_limit-Canones.at(0)->getPosy(),ui->doubleSpinBox_2->value(),ui->doubleSpinBox->value(),Canones.at(1)->getPosx(),1));
+        scene->addItem(Disparos.back());
+        Disparos.push_back(new Proyectil_Graph(Canones.at(0)->getPosx(),v_limit-Canones.at(0)->getPosy(),ui->doubleSpinBox_2->value(),ui->doubleSpinBox->value(),Canones.at(1)->getPosx(),3));
+        scene->addItem(Disparos.back());
+        move->start(100);
+    }
+    else if(Seed==4){
+        Simulacion_4(ui->doubleSpinBox_2->value(),ui->doubleSpinBox->value());
         Disparos.push_back(new Proyectil_Graph(Canones.at(0)->getPosx(),v_limit-Canones.at(0)->getPosy(),ui->doubleSpinBox_2->value(),ui->doubleSpinBox->value(),Canones.at(1)->getPosx(),1));
         scene->addItem(Disparos.back());
         Disparos.push_back(new Proyectil_Graph(Canones.at(0)->getPosx(),v_limit-Canones.at(0)->getPosy(),ui->doubleSpinBox_2->value(),ui->doubleSpinBox->value(),Canones.at(1)->getPosx(),3));
